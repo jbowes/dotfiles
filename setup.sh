f60b1e1297e583cca279ce68df28a39d7a60fa86
James Bowes <jbowes@repl.ca>
2013-04-23 07:08:56 -0300

    Add bash[rc|_profile]

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+ln -s ~/code/dotfiles/bashrc ~/.bashrc
+ln -s ~/code/dotfiles/bash_profile ~/.bash_profile
+
 ln -s ~/code/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/code/dotfiles/vimrc ~/.vimrc
