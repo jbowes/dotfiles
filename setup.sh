11152162730c08f2f42651c35af128fa60e18894
James Bowes <jbowes@repl.ca>
2013-04-23 07:11:57 -0300

    Add tigrc

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s ~/code/dotfiles/bashrc ~/.bashrc
 ln -s ~/code/dotfiles/bash_profile ~/.bash_profile
 
+ln -s ~/code/dotfiles/tigrc ~/.tigrc
 ln -s ~/code/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/code/dotfiles/vimrc ~/.vimrc
